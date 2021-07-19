3a78e75ea4a9114fd41ef5d3e3cb0ec16c6d2ce9
itdhwe <ken.hw.leung@fwd.com>
2021-07-19 21:51:06 +0800

    removed unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-	require 'sidekiq/web'
-	mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'orders#index'
 
